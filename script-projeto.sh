44096fd78b7569c07beace30ba25490180c28280
Júnior César Abreu <juniorcesarabreu@live.com>
2022-08-24 17:56:36 -0300

    Correção da senha

--- script-projeto.sh
+++ script-projeto.sh
@@ -40,31 +40,31 @@ echo "Configuração de diretórios concluída!"
 
 echo "Criando usuários..."
 
-useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd -6 $(openssl rand -base64 12)) -G GRP_ADM
+useradd carlos -c "Carlos" -s /bin/bash -m -p $(openssl passwd -6 senha123) -G GRP_ADM
 passwd carlos -e
 
-useradd maria -c "Maria" -s /bin/bash -m -p $(openssl passwd -6 $(openssl rand -base64 12)) -G GRP_ADM
+useradd maria -c "Maria" -s /bin/bash -m -p $(openssl passwd -6 senha123) -G GRP_ADM
 passwd maria -e
 
-useradd joao -c "João" -s /bin/bash -m -p $(openssl passwd -6 $(openssl rand -base64 12)) -G GRP_ADM
+useradd joao -c "João" -s /bin/bash -m -p $(openssl passwd -6 senha123) -G GRP_ADM
 passwd joao -e
 
-useradd debora -c "Débora" -s /bin/bash -m -p $(openssl passwd -6 $(openssl rand -base64 12)) -G GRP_VEN
+useradd debora -c "Débora" -s /bin/bash -m -p $(openssl passwd -6 senha123) -G GRP_VEN
 passwd debora -e
 
-useradd sebastiana -c "Sebastiana" -s /bin/bash -m -p $(openssl passwd -6 $(openssl rand -base64 12)) -G GRP_VEN
+useradd sebastiana -c "Sebastiana" -s /bin/bash -m -p $(openssl passwd -6 senha123) -G GRP_VEN
 passwd sebastiana -e
 
-useradd roberto -c "Roberto" -s /bin/bash -m -p $(openssl passwd -6 $(openssl rand -base64 12)) -G GRP_VEN
+useradd roberto -c "Roberto" -s /bin/bash -m -p $(openssl passwd -6 senha123) -G GRP_VEN
 passwd roberto -e
 
-useradd josefina -c "Josefina" -s /bin/bash -m -p $(openssl passwd -6 $(openssl rand -base64 12)) -G GRP_SEC
+useradd josefina -c "Josefina" -s /bin/bash -m -p $(openssl passwd -6 senha123) -G GRP_SEC
 passwd josefina -e
 
-useradd amanda -c "Amanda" -s /bin/bash -m -p $(openssl passwd -6 $(openssl rand -base64 12)) -G GRP_SEC
+useradd amanda -c "Amanda" -s /bin/bash -m -p $(openssl passwd -6 senha123) -G GRP_SEC
 passwd amanda -e
 
-useradd rogerio -c "Rogério" -s /bin/bash -m -p $(openssl passwd -6 $(openssl rand -base64 12)) -G GRP_SEC
+useradd rogerio -c "Rogério" -s /bin/bash -m -p $(openssl passwd -6 senha123) -G GRP_SEC
 passwd rogerio -e
 
 echo "Usuários criados!"
